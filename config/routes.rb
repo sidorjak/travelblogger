c3d936560b7c2555d88ef991035c64e41c9980da
Ivan Sidorjak <ivan.sidorjak@gmail.com>
2013-12-08 19:24:56 +0000

    Sessions for sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Travelblogger::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  root  'users#new'
+  
   match '/signup', to: 'users#new', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   
   
   # The priority is based upon order of creation: first created -> highest priority.
