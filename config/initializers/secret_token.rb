cf9cece222cc6e7931729fefecefd5828a0e0ec8
Ivan Sidorjak <ivan.sidorjak@gmail.com>
2013-12-08 15:49:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Travelblogger::Application.config.secret_key_base = '165212da238ad3272fb667c8251dd537adc8d1fb4e18ee671062158327373f220b50eaaf24e3b6d36d0816e96c9cac7135f2a071699606fe517c47d2047c5dec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Travelblogger::Application.config.session_store :cookie_store, key: '_travelblogger_session'
