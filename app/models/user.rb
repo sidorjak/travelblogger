4ca53d3207df2d5bdc54e0ea31e15559e53c487a
Ivan Sidorjak <ivan.sidorjak@gmail.com>
2013-12-08 18:49:56 +0000

    Added minimum password length, and SSL to app

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 end
